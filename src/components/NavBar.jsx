65bf07b58488f9be662b371fb89a99304fe26f44
Nawab Sheikh <nawabsheikh77472@gmail.com>
2025-09-19 07:11:56 +0530

    Added industries link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = ({className}) => {
         <h2 className="nav-links"><Link to="/">Home</Link></h2>
         <h2 className="nav-links"><Link to="/about">About Us</Link></h2>
         <h2 className="nav-links"><Link to="/products">Products</Link></h2>
-        <h2 className="nav-links"><Link to="/">Industries We Serve</Link></h2>
+        <h2 className="nav-links"><Link to="/industries">Industries We Serve</Link></h2>
         <h2 className="nav-links"><Link to="/">Business Operations</Link></h2>
       </div>
     </div>
